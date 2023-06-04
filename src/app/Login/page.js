ca6eab11f72bb2fffe8a5e0544a6fe25679d4fb9
sushiyade <tetzushi@gmail.com>
2023-06-04 19:52:16 +0800

    New style for login and signup

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -8,7 +8,7 @@ function Page() {
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
     const router = useRouter()
-    
+
     const handleForm = async (event) => {
         event.preventDefault()
 
@@ -35,7 +35,7 @@ function Page() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Create a new account
+                    Log in to your account
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleForm} >
                     <div>
--- src/app/SignUp/page.js
+++ src/app/SignUp/page.js
@@ -1,4 +1,5 @@
 'use client'
+import Link from "next/link";
 import React from "react";
 import signUp from "../Firebase/auth/signUp"
 import { useRouter } from 'next/navigation'
@@ -6,10 +7,16 @@ import { useRouter } from 'next/navigation'
 function Page() {
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
+    const [passwordrepeat, setPasswordRepeat] = React.useState('')
     const router = useRouter()
 
     const handleForm = async (event) => {
         event.preventDefault()
+
+        if (passwordrepeat != password) {
+            console.log("passwords do not match");
+            return;
+        }
 
         const { result, error } = await signUp(email, password);
 
@@ -22,22 +29,45 @@ function Page() {
         return router.push("/admin")
     }
     
-    return (<div className="wrapper">
-        <div className="form-wrapper">
-            <h1 className="mt-60 mb-30">Sign up</h1>
-            <form onSubmit={handleForm} className="form">
-                <label htmlFor="email">
-                    <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
-                </label>
-                <label htmlFor="password">
-                    <p>Password</p>
-                    <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
-                </label>
-                <button type="submit">Sign up</button>
-            </form>
+    return (
+        <div>
+            <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <img className="w-8 h-8 mr-2" src="https://i.ibb.co/mzz7Myk/2023-05-25-3-52-38.png" alt="logo"/>
+                Athlead    
+            </a>
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Create a new account
+                    </h1>
+                    <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleForm} >
+                        <div>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="NUSNETID@u.nus.edu" required />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="enter your password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        <div>
+                            <label htmlFor="passwordrepeat" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input onChange={(e) => setPasswordRepeat(e.target.value)} type="password" name="passwordrepeat" id="passwordrepeat" placeholder="enter your password again" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
+                    </form>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Already have an account? <Link href="/Login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</Link>
+                    </p>
+                </div>
+            </div>
         </div>
-    </div>);
-}
-
+    </section>
+        </div>
+    );
+};
+    
 export default Page;
+
+
