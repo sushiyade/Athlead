942963feffb1fc51b165132c7fccd8b8fcaec2f3
sushiyade <tetzushi@gmail.com>
2023-06-04 19:51:04 +0800

    New style for login and signup

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -8,6 +8,7 @@ function Page() {
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
     const router = useRouter()
+    
     const handleForm = async (event) => {
         event.preventDefault()
 
@@ -45,10 +46,6 @@ function Page() {
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                         <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="enter your password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                     </div>
-                    <div>
-                        <label htmlFor="passwordrepeat" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="passwordrepeat" id="passwordrepeat" placeholder="enter your password again" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    </div>
                     <div className="flex items-center justify-between">
                         <div className="flex items-start">
                             <div className="flex items-center h-5">
@@ -62,7 +59,7 @@ function Page() {
                     <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
                 </form>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <Link href="/Login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</Link>
+                      Don't have an account? <Link href="/SignUp" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                   </p>
             </div>
         </div>
