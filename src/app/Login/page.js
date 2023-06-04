485685194a325b3356ccd30df66c2f648b94165e
sushiyade <tetzushi@gmail.com>
2023-06-04 18:11:03 +0800

    New style

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,4 +1,5 @@
 'use client'
+import Link from "next/link";
 import React from "react";
 import signIn from "../Firebase/auth/signIn";
 import { useRouter } from 'next/navigation'
@@ -7,7 +8,6 @@ function Page() {
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
     const router = useRouter()
-
     const handleForm = async (event) => {
         event.preventDefault()
 
@@ -21,23 +21,55 @@ function Page() {
         console.log(result)
         return router.push("/admin")
     }
-    return (<div className="wrapper">
-        <div className="form-wrapper">
-            <h1 className="mt-60 mb-30">Sign in</h1>
-            <form onSubmit={handleForm} className="form">
-                <label htmlFor="email">
-                    <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
-                </label>
-                <label htmlFor="password">
-                    <p>Password</p>
-                    <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
-                </label>
-                <button type="submit">Sign in</button>
-            </form>
+    
+
+    return (
+      <div>
+        <section className="bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img className="w-8 h-8 mr-2" src="https://i.ibb.co/mzz7Myk/2023-05-25-3-52-38.png" alt="logo"/>
+            Athlead    
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Create a new account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleForm} >
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="NUSNETID@u.nus.edu" required />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="enter your password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                    <div>
+                        <label htmlFor="passwordrepeat" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" name="passwordrepeat" id="passwordrepeat" placeholder="enter your password again" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                              <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                            </div>
+                            <div className="ml-3 text-sm">
+                              <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
+                </form>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <Link href="/Login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</Link>
+                  </p>
+            </div>
         </div>
-
-    </div>);
-}
-
-export default Page;
+    </div>
+  </section>
+      </div>
+    );
+  };
+  
+  export default Page;
